b04007df073905a83a375f261052b8551443e4bc
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-12-11 23:00:16 +0100

    Add soft deletion to all models

--- src/app/Models/CollectionEntity.php
+++ src/app/Models/CollectionEntity.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @mixin Builder
@@ -20,7 +21,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class CollectionEntity extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
