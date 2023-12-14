08c2dbdfab234a587693ace94f6d146894d1ff88
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-12-14 17:53:40 +0100

    Remove unnecessary margin

--- src/resources/views/users/change_password.blade.php
+++ src/resources/views/users/change_password.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-card class="p-10 max-w-lg mx-auto mt-24">
         <div
-            class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
+            class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto"
         >
             <header class="text-center">
                 <h2 class="text-2xl font-bold uppercase mb-4">
