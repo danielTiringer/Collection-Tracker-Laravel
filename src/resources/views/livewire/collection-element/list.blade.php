4d79c71eb2b2032c7771d621bc9ee90d6ce1aea1
Daniel Tiringer <tiringerdaniel@tutanota.com>
2024-01-01 17:40:49 +0100

    Move the search out of the unless

--- src/resources/views/livewire/collection-element/list.blade.php
+++ src/resources/views/livewire/collection-element/list.blade.php
@@ -1,18 +1,19 @@
 <div>
-    @unless(count($elements) == 0)
-        <div class="flex items-center mt-4">
+    <div class="flex items-center mt-4">
+        <div class="w-full" x-data="{ search: '{{ request()->get('search') }}' }">
             <label for="voice-search" class="sr-only">Search</label>
-            <div class="w-full" x-data="{ search: '{{ request()->get('search') }}' }">
-                <input
-                    id="voice-search"
-                    type="text"
-                    name="search"
-                    wire:model.live="searchQuery"
-                    class="h-14 w-full px-6 rounded-lg bg-gray-50 border border-gray-200 focus:shadow focus:outline-none"
-                    placeholder="Search in collection"
-                />
-            </div>
+            <input
+                id="voice-search"
+                type="text"
+                name="search"
+                wire:model.live="searchQuery"
+                class="h-14 w-full px-6 rounded-lg bg-gray-50 border border-gray-200 focus:shadow focus:outline-none"
+                placeholder="Search in collection"
+            />
         </div>
+    </div>
+
+    @unless(count($elements) == 0)
 
         <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mt-4">
             @foreach($elements as $element)
